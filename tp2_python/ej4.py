eadcd68bde228570a8f53ffc1a00c0a6869dc191
gestral <dcolque2025@gmail.com>
2025-06-06 06:15:47 +0000

    Name changed

---
+++ tp2_python/ej4.py
@@ -0,0 +1,7 @@
+# 4. Crear una variable BASE y otra ALTURA, calcular y mostrar el área de un
+# triángulo (base x altura /2).
+
+a = int(input("Ingresar variable altura: "))
+b = int(input("Ingresar variable base: "))
+
+print(f"Resultado: {(a+b)/2}")
